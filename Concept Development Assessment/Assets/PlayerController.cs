564756ab3b7bf0d515acd6fa0784a18217e0ecb1
Sam Chandler <36247953+Sam-Chan02@users.noreply.github.com>
2023-01-11 17:06:23 +0000

    Added Villager to save

--- Concept Development Assessment/Assets/PlayerController.cs
+++ Concept Development Assessment/Assets/PlayerController.cs
@@ -2,55 +2,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
     public GameObject player;
+    public InputActionAsset inputController;
+    public List<GameObject> villagers = new List<GameObject>();
     public float maxSpeed = 0.2f;
-    public float acceleration = 0.02f;
+    public float acceleration = 0.04f;
+    public GameObject followPos;
+    private List<Vector2> storedPosition;
+    private InputAction moveInput;
     private Rigidbody2D rb;
+    private float velocity = 0;
     private float moveDirection;
-    private float speedMag = 0;
-    private float speed = 0;
-    private bool moving = false;
+    private float direction;
+    private int jumps = 1;
+    private bool airborne = false;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        moveInput = inputController.FindAction("Move");
         transform.position = new Vector2(0f, 0f);
+        storedPosition = new List<Vector2>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(moving);
+        storedPosition.Add(transform.position);
+
+        if(storedPosition.Count > 30)
+        {
+            followPos.transform.position = storedPosition[0];
+            storedPosition.RemoveAt(0);
+        }
     }
 
     private void FixedUpdate()
-    {
-        transform.Translate(new Vector2(speed, 0));
+    { 
+        if (moveDirection != 0)
+        {
+            direction = moveDirection;
+            velocity += acceleration * direction;
+            if (Math.Abs(velocity) > maxSpeed)
+            {
+                velocity = maxSpeed * direction;
+            }
+        }
+        else if (Math.Abs(velocity) > 0)
+        {
+            velocity -= acceleration * direction;
+            if (Math.Abs(velocity) < 0.02)
+            {
+                velocity = 0;
+            }
+        }
+        transform.Translate(new Vector2(velocity, 0));
     }
 
     void OnMove(InputValue direction)
     {
-        moving = true;
         moveDirection = direction.Get<float>();
-        speedMag += acceleration;
-        if (speedMag > maxSpeed)
-        {
-            speedMag = maxSpeed;
-        }
-        speed = speedMag * moveDirection;
     }
 
     void OnJump()
     {
-
+        if (jumps > 0)
+        {
+            jumps--;
+            airborne = true;
+            rb.AddForce(new Vector2(0, 300));
+        }
     }
 
     private void OnCrouch()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        airborne = false;
+        jumps = 1;
     }
 }
---
+++ Concept Development Assessment/Assets/VillagerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillagerController : MonoBehaviour
+{
+    public GameObject followPos;
+    private GameObject player;
+    private bool following = false;
+    private List<Vector2> storedPosition;
+    private List<GameObject> villagerList;
+    private Rigidbody2D rb;
+    private bool dead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        villagerList = player.GetComponent<PlayerController>().villagers;
+        storedPosition = new List<Vector2>();
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+        dead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!following && Vector2.Distance(transform.position, player.transform.position) < 1.5f)
+        {
+            villagerList.Add(this.gameObject);
+            following = true;
+            Physics2D.IgnoreCollision(player.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+        if (following)
+        {
+            storedPosition.Add(transform.position);
+
+            if (storedPosition.Count > 30)
+            {
+                followPos.transform.position = storedPosition[0];
+                storedPosition.RemoveAt(0);
+            }
+        }
+        if (transform.position.y < -8 && dead)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (following && !dead)
+        {
+            if (villagerList.IndexOf(this.gameObject) == 0)
+            {
+                transform.position = (player.transform.GetChild(0).position - new Vector3(0,0.65f, 0));
+            }
+            else
+            {
+                transform.position = villagerList[villagerList.IndexOf(this.gameObject) - 1].transform.GetChild(0).position;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Enemy")
+        {
+            villagerList.RemoveAt(villagerList.IndexOf(this.gameObject));
+            dead = true;
+            GetComponent<Collider2D>().enabled = false;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector2(0, 500));
+        }
+    }
+}
