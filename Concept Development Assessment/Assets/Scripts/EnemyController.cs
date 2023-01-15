a803304a746f4441561b4e11c5c23609c906a20e
Sam Chandler <36247953+Sam-Chan02@users.noreply.github.com>
2023-01-15 15:40:45 +0000

    enemy movement

--- Concept Development Assessment/Assets/Scripts/EnemyController.cs
+++ Concept Development Assessment/Assets/Scripts/EnemyController.cs
@@ -7,11 +7,13 @@ public class EnemyController : MonoBehaviour
     public int health = 1;
     private Rigidbody2D rb;
     public float enemySpeed = 2;
-    private float direction = -1;
+    private float direction = 1;
+    private SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -26,5 +28,14 @@ public class EnemyController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(enemySpeed * direction, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag != "Player")
+        {
+            sr.flipX = direction < 0;
+            direction = -direction;
+        }
     }
 }
